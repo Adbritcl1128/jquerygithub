988fab226eee092fb880c64f73620f7b58f1b046
Alex Britcliffe <alexanderbritcliffe@gmail.com>
2016-11-02 13:22:56 -0400

    javascript

---
+++ Javascript/jquery.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+
+  window.fee = window.fee || {}:
+
+
+
+
+$('button').on('click', function Data(event){
+  var token = $('token').val ()
+  window.fee.token = myToken;
+$.ajax({
+  url: 'https://api.github.com/users/jakerella',
+  method: 'GET',
+  dataType: 'json'
+  headers: {
+    Authorization: 'token' + token '187bf9ce05bdae03ec1858d2ecc60b5f736bccbe';
+  }
+})
+.done(function succesHandler(data) {
+  data.forEach
+
+
+
+}());
